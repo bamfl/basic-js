127e28f350cb99385a6275682d17f4ef408ab328
Dmitriy Petukhov <pdnsite@gmail.com>
2021-09-11 11:44:30 +0300

    feat: dream-team task is ready

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,15 +2,31 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
+export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
 
+  let teamName = [];
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] !== 'string') {
+      continue;
+    }
+
+    teamName.push(members[i].trim()[0].toUpperCase());
+  }
+
+  return teamName.sort().join('');
+}
