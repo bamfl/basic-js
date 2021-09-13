3827d4d7570fb68669ec2ed3eaeab25bdb2be70f
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2021-09-13 10:03:42 +0300

    feat: add extend-repeater task solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,20 +2,28 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' } = options;
+
+  str += '';
+  addition += '';
+
+  const additionStr = new Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+
+  return new Array(repeatTimes).fill(str + additionStr).join(separator);
 }
+
+console.log(repeater('la', { repeatTimes: 3 }));
